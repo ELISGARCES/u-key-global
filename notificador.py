200e216532752c421d3050681e9633c9870a6879
ELISGARCES <elisgarces1966@gmail.com>
2026-03-17 06:24:04 -0400

    Add files via upload

--- notificador.py
+++ notificador.py
@@ -3,6 +3,7 @@ import socketserver
 import json
 import smtplib
 import random
+import os  # <-- Esto es nuevo para Render
 from email.message import EmailMessage
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
@@ -18,20 +19,17 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
         post_data = self.rfile.read(content_length)
         data = json.loads(post_data)
         
-        # Generar código de 6 dígitos
         codigo = str(random.randint(100000, 999999))
         
-        # CONFIGURA AQUÍ TU GMAIL
-        remitente = "elisgarces1966@gmail.com"
-        password = "yyuy yugv tjbh fkms"
-
-
+        # COLOCA TUS DATOS AQUÍ DE NUEVO (CON COMILLAS)
+        remitente = "elisgarces1966@gmail.com" 
+        password = "tu_clave_de_16_letras_aqui"
         
         msg = EmailMessage()
         msg['Subject'] = f'Tu Código U-KEY: {codigo}'
         msg['From'] = remitente
         msg['To'] = data['email']
-        msg.set_content(f"Hola {data['nombre']},\n\nTu código de acceso seguro es: {codigo}")
+        msg.set_content(f"Hola {data['nombre']},\n\nTu código de acceso seguro a U-KEY es: {codigo}")
 
         try:
             with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
@@ -43,10 +41,12 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
             self.send_header('Content-Type', 'application/json')
             self.end_headers()
             self.wfile.write(json.dumps({"codigo_servidor": codigo}).encode())
-        except Exception as e:
+        except Exception:
             self.send_response(500)
             self.end_headers()
 
-with socketserver.TCPServer(("", 8080), MyHandler) as httpd:
-    print("Servidor U-KEY activo en el puerto 8080")
+# Esta línea es la que permite que Render funcione correctamente
+puerto = int(os.environ.get("PORT", 8080))
+with socketserver.TCPServer(("", puerto), MyHandler) as httpd:
+    print(f"Servidor U-KEY Activo en Puerto {puerto}")
     httpd.serve_forever()
